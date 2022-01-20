be046b347dc78a2e0f30b5728c23ed426af0b90e
Ibrahimi20 <71458263+Ibrahimi20@users.noreply.github.com>
2022-01-20 10:30:33 +0100

    by hicham

---
+++ precache-manifest.d17727bf1a88fd93a862291bc74eecc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb84299799a53cb314d4b1f4a4ce6802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d5523abc7c5d8b83248",
+    "url": "/static/css/main.0ac238b4.chunk.css"
+  },
+  {
+    "revision": "6b8f398b0363e0d8a5fc",
+    "url": "/static/js/2.92046c4e.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/static/js/2.92046c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5523abc7c5d8b83248",
+    "url": "/static/js/main.451c271a.chunk.js"
+  },
+  {
+    "revision": "7a4329af9fa263220141",
+    "url": "/static/js/runtime-main.8326d2ee.js"
+  },
+  {
+    "revision": "1be842dbbe714f4e705c4416e90eaec9",
+    "url": "/static/media/Artboard 1.1be842db.png"
+  },
+  {
+    "revision": "9ef4155633103ab8233c6d89718a4520",
+    "url": "/static/media/microphoneIcon.9ef41556.svg"
+  }
+]);
